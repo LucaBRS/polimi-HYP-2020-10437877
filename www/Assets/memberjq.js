11577d89f1b23ff3421e4be995e2d6d1949caf41
Luca Barsottini <luca.barsottini@mail.polimi.it>
2020-06-04 20:14:09 +0200

    polishing

--- www/Assets/memberjq.js
+++ www/Assets/memberjq.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 
 function makemember(data) {
     member = data;
-    $('#foundname1').append(member[0].first_name + '<pre>' + member[0].last_name+'</pre>')
-    $('#foundname2').append(member[1].first_name + '<pre>' + member[1].last_name+'</pre>')
+    $('#foundname1').append(member[0].first_name + '<p>' + member[0].last_name+'</p>')
+    $('#foundname2').append(member[1].first_name + '<p>' + member[1].last_name+'</p>')
     $('#img1').append('<img height=100% width=100%  src="'+member[1].member_photo + '" alt="picture"></img>')
     $('#img2').append('<img height=100% width=100% src="'+member[2].member_photo + '  " alt="picture"></img>')
     $('#img3').append('<img height=100% width=100% src="'+member[3].member_photo + '  " alt="picture"></img>')
@@ -20,12 +20,12 @@ function makemember(data) {
 
 
 
-    $('#foundname3').append(member[2].first_name + '<pre>' + member[2].last_name+'</pre>')
+    $('#foundname3').append(member[2].first_name + '<p>' + member[2].last_name+'</p>')
 
-    $('#vet1').append(member[3].first_name + '<pre>' + member[3].last_name+'</pre>')
-    $('#vet2').append(member[4].first_name + '<pre>' + member[4].last_name+'</pre>')
+    $('#vet1').append(member[3].first_name + '<p>' + member[3].last_name+'</p>')
+    $('#vet2').append(member[4].first_name + '<p>' + member[4].last_name+'</p>')
 
-    $('#vet3').append(member[5].first_name + '<pre>' + member[5].last_name+'</pre>')
+    $('#vet3').append(member[5].first_name + '<p>' + member[5].last_name+'</p>')
     
 
     $('#id').append('<p>' + member[2] + '</p>')

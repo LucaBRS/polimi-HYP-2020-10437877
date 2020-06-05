6e8a4a67a0b8c40bdf1888da40d7e92a18679c61
Luca Barsottini <luca.barsottini@mail.polimi.it>
2020-06-05 13:51:09 +0200

    photo  position and text removal

--- www/Assets/id_eventjq.js
+++ www/Assets/id_eventjq.js
@@ -19,7 +19,8 @@ function makeEvent(data){
             for(event of data)
             $('#evna').append('<h3 class="text-center tert">'+event.event_name+'</h3><p class="text-center">'+event.event_year+'</p>')
 
-                $('#imev').append('<img width=100% height=auto alt="img" src="'+event.event_photo+'"</img>')
+                $('#imev').append('<img width=100% height=auto class="hidden-mobile" alt="img" src="'+event.event_photo+'"</img>'+
+                '<img height=auto width=100% class=" pt-5   visible-mobile" src="'+event.event_photo+'" alt="a dog ">')
                 $('#evtext').append('<p class="display-5 pl-5">'+event.event_description+'</p>')
 
                 $('#evdate').append('<i class="fa fa-calendar " style="font-size:24px">'+event.event_day+'/'+event.event_month+'/'+event.event_year+' </i>')
@@ -30,7 +31,8 @@ function makeEvent(data){
 
                 $('#servname').append('<h1>'+event.service_name+'</h1>')
                 $('#servinf').append('<p>'+event.other_info+'</p>')
-                $('#servph').append('<img class="hidden-mobile" width=100% height=100% alt="img" src="'+event.service_photo+'"</img>')
+                $('#servph').append('<img class="hidden-mobile" width=100% height=100% alt="img" src="'+event.service_photo+'"</img>'+
+                '<img height=auto width=100% class=" pt-5   visible-mobile" src="'+event.service_photo+'" alt="a dog ">')
                 $('#servlink').append('<a class="btn bor" href="../static/id_service.html?id='+event.id_service + '">' +'About rescue'+'</a>')
 
 
